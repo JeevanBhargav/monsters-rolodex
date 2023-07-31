48a9f876a9e1c57f2f29e5b701d6e15d98d6d07c
Jeevan Bhargav <g.jeevanbhargav@gmail.com>
2023-07-31 13:06:09 -0600

    Till 32 setState and Secondary

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
+import{Component} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      name: {firstName:'Yihua', lastName:'Zhang'}, company: 'ZTM' //key value pair
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I Work at {this.state.company}
+          </p>
+          <button onClick={() => {
+            this.setState(
+              () => {
+              return {
+                name: {firstName:'Jeevan', lastName:'Bhar'},
+              }
+            },
+            () => {
+              console.log(this.state);
+            }
+            );
+            //console.log(this.state);//Instead of this
+          }}
+          
+          >
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
